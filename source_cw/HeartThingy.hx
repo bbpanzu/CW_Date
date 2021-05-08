3cd477095d9a553a9e828c336673ba699a34afbf
bbpanzu <69420+bbpanzu@users.noreply.github.com>
2021-05-08 10:47:16 -0400

    da whole shits

---
+++ source_cw/HeartThingy.hx
@@ -0,0 +1,46 @@
+package;
+import flixel.*;
+
+/**
+ * ...
+ * @author bbpanzu
+ */
+class HeartThingy extends FlxSprite 
+{
+	public var fadedelay:Int = 0;
+	public var framee:Int = 0;
+	public var speed:Float = 0;
+	public function new(X,Y,SPEED:Float=1) 
+	{
+		super(X,Y);
+		speed = SPEED;
+		fadedelay = FlxG.random.int(240, 360);
+		frames = Paths.getSparrowAtlas("date/heartthingy");
+		animation.addByPrefix("bump", "heartthing", 24, true);
+		scrollFactor.set(0.8,0.8);
+		antialiasing = true;
+		active = true;
+		blend = "overlay";
+		trace("added heart");
+		
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		
+		y-= speed;
+		framee ++;
+		
+		if (framee > fadedelay){
+			alpha -= 0.01;
+		}
+		
+		animation.play("bump"); 
+		if (alpha > 0){
+			super.update(elapsed);
+		}else{
+			visible = false;
+		}
+	}
+	
+}
